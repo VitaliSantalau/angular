05f53f1ec9493ce98de3308456b846db282bbed6
VitaliSantalau <v.santalau@gmail.com>
2022-03-10 07:31:12 +0300

    feat: add heroes list

--- heroes/src/app/heroes/heroes.component.ts
+++ heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './heroes';
+import { HeroService } 
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Winstorm'
-  }
 
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
